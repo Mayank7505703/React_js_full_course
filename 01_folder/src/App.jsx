c1245c1988aa80bcc1d534483209dc07af3c45e1
Mayank7505703 <karansharma90900@gmail.com>
2026-02-02 00:30:52 +0530

    01_folder commited

---
+++ 01_folder/src/App.jsx
@@ -0,0 +1,16 @@
+const App=()=>{
+  return (
+    // fragments
+    <>
+    <div className="dad">
+      <h1 id="child1">Hello</h1>
+      <h2 id="child2">Hello</h2>
+      </div>
+      <div className="chacha">
+      <h1 id="child3">Hello</h1>
+      <h2 id="child4">Hello</h2>
+      </div>
+    </>
+  )
+}
+export default App;
